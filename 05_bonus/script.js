b9da0d510b86acacc6dfe4c3342b4e07e4baa3eb
simoneburrai <burrai.s98@gmail.com>
2025-03-03 14:23:39 +0100

    feat: 05_bonus_completed

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,26 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const personName = 'Mario';
+
 
 
 // Dichiara la funzione qui.
-
+function greetingWithName (name){
+    const date = new Date();
+    let hour = date.getHours();
+    if (hour >= 0 && hour < 13){
+        console.log(`Buongiorno ${name}, il buongiorno si vede dal mattino`);
+    } else if (hour >= 13 && hour < 17){
+        console.log(`Buon Pomeriggio ${name}, oggi fai una siesta?`);
+    } else if (hour >= 17 && hour <= 23){
+        console.log(`Buonasera ${name}, hai già cenato?`);
+    }
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+greetingWithName(personName);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
