fef2cd83f01055ec848c0db0c8f5a18ccf491425
simoneburrai <burrai.s98@gmail.com>
2025-03-03 13:26:06 +0100

    01_saluti completed, 01_saluti_arrow completed

--- 01_saluti_arrow/script.js
+++ 01_saluti_arrow/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome 
 e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
+const userName = 'Mario'; 
 
 
 // Dichiara la funzione qui.
-function greetingWithName (name){
+const greetingWithName = name => {
     console.log(`Ciao ${name}, è un piacere conoscerti`);
 }
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
